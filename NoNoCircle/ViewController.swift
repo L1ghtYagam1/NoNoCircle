e288b6a89aee84f1b28c8b525b7d4c782fcbaa1b
L1ghtYagam1 <Yaska2608@mail.ru>
2021-12-09 23:25:58 +0300

    1

--- NoNoCircle/ViewController.swift
+++ NoNoCircle/ViewController.swift
@@ -9,11 +9,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var lightRed: UIView!
+    @IBOutlet weak var lightYeloy: UIView!
+    @IBOutlet weak var lightGren: UIView!
+    
+    @IBOutlet weak var startButton: UIButton!
+    
+    
+    private let lightIsOn: CGFloat = 1
+    private let lightIsOff: CGFloat = 0.3
+    
+    var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        lightRed.layer.cornerRadius = 70
+        lightYeloy.layer.cornerRadius = 70
+        lightGren.layer.cornerRadius = 70
+        
+        startButton.layer.cornerRadius = 10
+        
+        lightRed.alpha = 0.3
+        lightYeloy.alpha = 0.3
+        lightGren.alpha = 0.3
     }
+    
+    
+    
+    @IBAction func startButton(_ sender: Any) {
+    if startButton.currentTitle == "START" {
+            startButton.setTitle("NEXT", for: .normal)
+        }
+        switch count {
+        case 0:
+            lightRed.alpha = lightIsOn
+            lightGren.alpha = lightIsOff
+            count = 1
+        case 1:
+            lightYeloy.alpha = lightIsOn
+            lightRed.alpha = lightIsOff
+            count = 2
+        case 2:
+            lightYeloy.alpha = lightIsOff
+            lightGren.alpha = lightIsOn
+            count = 0
+        default:
+            break
+    
+        }
 
-
+      }
+    
+    
+    
 }
-
